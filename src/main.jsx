92cb5ad267e9cb5df378df872311c6f24b93f7e5
James Jegede <jexzyjames@gmail.com>
2025-05-03 13:10:55 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ContextProvider from './Context.jsx'
-import { AuthProvider } from './AuthProvider.jsx'
+// import ContextProvider from './Context.jsx'
+// import { AuthProvider } from './AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <ContextProvider>
-    <AuthProvider>
+  
 
     <App />
-    </AuthProvider>
-  </ContextProvider>,
+   
 )
