6e824d0dd0ca33f6bc6461f925c053cb75de1f52
James Jegede <jexzyjames@gmail.com>
2025-05-03 18:05:30 +0100

    Update Navbar.jsx

--- src/WasteGrid/components/Navbar.jsx
+++ src/WasteGrid/components/Navbar.jsx
@@ -7,11 +7,11 @@ const Navbar = () => {
         <img className='w-20' src={logo} alt="logo" />
      </div>
      <ul className='md:flex gap-4 hidden border rounded-2xl px-3 border-green-200 text-gray-700'>
-        <li>About</li>
-        <li>How It Works</li>
-        <li>Features</li>
-        <li>Rewards</li>
-        <li>FAQ</li>
+        <li><a href="#about">About</a></li>
+        <li><a className='text-gray-300 cursor-pointer' href="#">How It Works</a></li>
+        <li><a className='text-gray-300 cursor-pointer' href="#features">Features</a></li>
+        <li><a className='text-gray-300 cursor-pointer' href="#">Rewards</a></li>
+        <li><a className='text-gray-300 cursor-pointer' href="#">FAQ</a></li>
      </ul>
      <div className='rounded-3xl py-2 px-4 text-sm text-white bg-green-400'>
         <button>Get The App</button>
