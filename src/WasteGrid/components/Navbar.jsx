692af41527b102a284afd0d2d20672ee885fb0d9
jexzyjames <jexzyjames@gmail.com>
2025-05-06 22:31:36 +0100

    imported menuIcon

--- src/WasteGrid/components/Navbar.jsx
+++ src/WasteGrid/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import logo from '../assets/logo.png'
+import menuIcon from '../assets/menuicon.png'
 const Navbar = () => {
   return (
     <div className='flex h-20  px-[4%] py-2  fixedNav shadow-md   items-center justify-between w-full '>
      <div>
         <a href='#'>
-
+    
         <img className='w-20 cursor-pointer ' src={logo} alt="logo" />
         </a>
         
@@ -17,8 +18,11 @@ const Navbar = () => {
         <li><a className='text-gray-300 cursor-pointer' href="#">Rewards</a></li>
         <li><a className='text-gray-300 cursor-pointer' href="#">FAQ</a></li>
      </ul>
-     <div className='rounded-3xl  hover:bg-green-500 hover:font-semibold cursor-pointer py-2 px-4 text-sm text-white bg-green-400'>
+     <div className='rounded-3xl hidden md:block  hover:bg-green-500 hover:font-semibold cursor-pointer py-2 px-4 text-sm text-white bg-green-400'>
         <button className='cursor-pointer' >Get The App</button>
+     </div>
+     <div className='md:hidden cursor-pointer flex '>
+      <img src={menuIcon} alt="" />
      </div>
     </div>
   )
